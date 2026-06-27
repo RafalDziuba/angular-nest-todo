a227cab5f0520227e2c090b27c886454bc4ecfa8
Rafal Dziuba <dziuba.rafal98@gmail.com>
2026-06-27 03:08:57 +0200

    feat: add registration DTO rule - one special character

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength, Matches } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail({}, { message: 'Niepoprawny format adresu e-mail' })
@@ -6,6 +6,10 @@ export class RegisterDto {
   email: string;
 
   @MinLength(6, { message: 'Hasło musi mieć co najmniej 6 znaków' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])/, {
+    message:
+      'Hasło musi zawierać co najmniej jedną małą literę, jedną wielką literę, jedną cyfrę oraz jeden znak specjalny',
+  })
   @IsNotEmpty({ message: 'Hasło nie może być puste' })
   password: string;
 }
